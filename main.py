7bff733ce3c55407ec7bc9a2774e40ed2d6525c5
Nazran <s3693595@student.rmit.edu.au>
2020-04-26 23:21:35 +1000

    New HTML Design

---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+# http://localhost:5000/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # This is where your GOOGLE MySQL code need to come
+        # Begin
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # Ends
+        # account = cursor.fetchone() is important its collecting data from the SQL execution. and sending to the line number 28
+        # to make a if else statement
+
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+
+# http://localhost:5000/logout - this will be the logout page
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+# http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+
+        # This is where your GOOGLE MySQL code need to come
+        # Begin
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # Ends
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+# http://localhost:5000/home - this will be the home page, only accessible for loggedin users
+@app.route('/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
