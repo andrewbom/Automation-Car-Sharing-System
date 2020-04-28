00b8245ee93595aada2696d7da4c18b341fc4bf8
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-04-28 19:56:07 +1000

    updated

---
+++ database_utils.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import pymysql  # MySQLdb  # pymysql
+
+
+class DatabaseUtils:
+    app = Flask(__name__)
+
+    HOST = "35.201.23.126"  # google cloud IP address
+    USER = "root"  # google cloud sql user name
+    PASSWORD = "andrewishandsome"  # google cloud sql password
+    DATABASE = "Pythonlogin"
+
+    def __init__(self, connection=None):
+        if connection is None:
+            connection = pymysql.connect(DatabaseUtils.HOST, DatabaseUtils.USER,
+                                         DatabaseUtils.PASSWORD, DatabaseUtils.DATABASE)
+        self.connection = connection
+
+    def close(self):
+        self.connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def create_account_table(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS `accounts` (
+                    `id` int(11) NOT NULL AUTO_INCREMENT,
+                    `username` varchar(50) NOT NULL,
+                    `password` varchar(255) NOT NULL,
+                    `email` varchar(100) NOT NULL,
+                    PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            """)
+
+        self.connection.commit()
+
+    def insert_account(self, username, password, email):
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO accounts VALUES (NULL, %s, %s, %s)", (username, password, email,))
+        self.connection.commit()
+
+        return cursor.rowcount == 1
+
+    def check_exist_username(self, username):
+        with self.connection.cursor() as cursor:
+            cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+
+        return cursor.fetchone()
+
+    def login_account(self, username, password):
+        with self.connection.cursor() as cursor:
+            cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM accounts WHERE username = %s AND password = %s", (username, password,))
+
+        return cursor.fetchone()
+
+    def get_account(self, id):
+        with self.connection.cursor() as cursor:
+            cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM accounts WHERE id = %s", (id,))
+
+        return cursor.fetchone()
+
+    def delete_account(self, id):
+        with self.connection.cursor() as cursor:
+            cursor.execute("delete from accounts where id = %s", id)
+        self.connection.commit()
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from database_utils import DatabaseUtils
+import re
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
-# http://localhost:5000/ - this will be the login page, we need to use both GET and POST requests
-@app.route('/', methods=['GET', 'POST'])
+
+def main(self):  # add error handling
+    with DatabaseUtils() as db:
+        db.create_account_table()
+
+
+# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/')
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
     msg = ''
@@ -12,35 +22,26 @@ def login():
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
+        with DatabaseUtils() as db:
+            account = db.login_account(username, password)
 
-        # This is where your GOOGLE MySQL code need to come
-        # Begin
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # Ends
-        # account = cursor.fetchone() is important its collecting data from the SQL execution. and sending to the line number 28
-        # to make a if else statement
-
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return 'Logged in successfully!'
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+            # If account exists in accounts table in out database
+            if account:
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                # Redirect to home page
+                return redirect(url_for('home'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
-    return render_template('index.html', msg=msg)
+    return render_template('index.html', msg='')
 
 
-# http://localhost:5000/logout - this will be the logout page
-@app.route('/logout')
+# http://localhost:5000/python/logout - this will be the logout page
+@app.route('/pythonlogin/logout')
 def logout():
     # Remove session data, this will log the user out
     session.pop('loggedin', None)
@@ -49,21 +50,34 @@ def logout():
     # Redirect to login page
     return redirect(url_for('login'))
 
-# http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/register', methods=['GET', 'POST'])
+
+# http://localhost:5000/pythinlogin/register - this will be the registration page,
+# we need to use both GET and POST requests
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         # Create variables for easy access
-
-        # This is where your GOOGLE MySQL code need to come
-        # Begin
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        # Ends
+
+        with DatabaseUtils() as db:
+            account = db.check_exist_username(username)
+            # If account exists show error and validation checks
+            if account:
+                msg = 'Account already exists!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers!'
+            elif not username or not password or not email:
+                msg = 'Please fill out the form!'
+            else:
+                db.insert_account(username, password, email)
+                msg = 'You have successfully registered!'
 
     elif request.method == 'POST':
         # Form is empty... (no POST data)
@@ -71,8 +85,9 @@ def register():
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
 
-# http://localhost:5000/home - this will be the home page, only accessible for loggedin users
-@app.route('/home')
+
+# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
+@app.route('/pythonlogin/home')
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
@@ -80,6 +95,23 @@ def home():
         return render_template('home.html', username=session['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        print(session['id'])
+        with DatabaseUtils() as db:
+            account = db.get_account(session['id'])
+
+            # Show the profile page with account info
+            return render_template('profile.html', account=account)
+
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
