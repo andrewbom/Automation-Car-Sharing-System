c0bfceec710bafc7e8e59a63314d2ac5c73cc6aa
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-05-02 09:01:19 +1000

    update

--- database_utils.py
+++ database_utils.py
@@ -5,9 +5,9 @@ import pymysql  # MySQLdb  # pymysql
 class DatabaseUtils:
     app = Flask(__name__)
 
-    HOST = "34.87.232.2"  # google cloud IP address
+    HOST = "35.201.23.126"  # google cloud IP address
     USER = "root"  # google cloud sql user name
-    PASSWORD = "123456789"  # google cloud sql password
+    PASSWORD = "andrewishandsome"  # google cloud sql password
     DATABASE = "Pythonlogin"
 
     def __init__(self, connection=None):
@@ -27,11 +27,10 @@ class DatabaseUtils:
 
     def create_account_table(self):
         with self.connection.cursor() as cursor:
-            # cursor.execute("DROP TABLE customers;")
+            cursor.execute("DROP TABLE customers;")
             cursor.execute("DROP TABLE cars_list;")
             cursor.execute("DROP TABLE bookings;")
 
-            #### I removed the username field
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS `customers` (
                     `customer_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -47,8 +46,10 @@ class DatabaseUtils:
             cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'John', 'Mathew', 'john@gmail.com', '123');")
             cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Anna', 'Williams', 'anna@gmail.com', '123');")
             cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Harry', 'Robert', 'harry@gmail.com', '123');")
-            cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Charlie', 'William', 'carlie@gmail.com', '123');")
-            cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Oliver', 'Michelle', 'oliver@gmail.com', '123');")
+            cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Charlie', 'William', 'carlie@gmail.com', "
+                           "'123');")
+            cursor.execute("INSERT IGNORE INTO `customers` VALUES (NULL, 'Oliver', 'Michelle', 'oliver@gmail.com', "
+                           "'123');")
 
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS `cars_list` (
@@ -59,18 +60,20 @@ class DatabaseUtils:
                     `seating_capacity` varchar(1) DEFAULT NULL,
                     `car_type` int(1) DEFAULT NULL COMMENT '1:Sedan | 2:Hatch | 3:SUV',
                     `price_per_km` decimal(10,2) NOT NULL,
-                    `status` varchar(10) NOT NULL,
+                    `status` varchar(15) NOT NULL,
                     UNIQUE(`registration_no`),
                     PRIMARY KEY (`car_id`)                    
                 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
             """)
-            cursor.execute("INSERT IGNORE INTO `cars_list` VALUES (NULL, 'Toyota', 'Camry', '1AB 2CD', '4', 1, '15', 'Available');")
-            cursor.execute("INSERT IGNORE INTO `cars_list` VALUES (NULL, 'Mazda', 'CX-5', '2YM 5CD', '4', 3, '20', 'Available');")
+            cursor.execute("INSERT IGNORE INTO `cars_list` VALUES (NULL, 'Toyota', 'Camry', '1AB 2CD', '4', 1, '15', "
+                           "'Not Available');")
+            cursor.execute("INSERT IGNORE INTO `cars_list` VALUES (NULL, 'Mazda', 'CX-5', '2YM 5CD', '4', 3, '20', "
+                           "'Available');")
 
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS `bookings` (
                     `booking_id` int(11) NOT NULL AUTO_INCREMENT,
-                    `user_id` int(11) NOT NULL,
+                    `customer_id` int(11) NOT NULL,
                     `car_id` int(11) NOT NULL,
                     `start_date` varchar(30) NOT NULL,
                     `end_date` varchar(30) NOT NULL,
@@ -81,19 +84,22 @@ class DatabaseUtils:
                     PRIMARY KEY (`booking_id`)                  
                 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
             """)
-            cursor.execute("INSERT IGNORE INTO `bookings` VALUES (NULL, 1, 2, '07-04-2020', '10-04-2020', '13:45', '155.00', 'booked', NULL);")
-            cursor.execute("INSERT IGNORE INTO `bookings` VALUES (NULL, 1, 2, '10-03-2020', '15-03-2020', '13:45', '123.00', 'Canceled', NULL);")
+            cursor.execute("INSERT IGNORE INTO `bookings` VALUES (NULL, 1, 1, '07-04-2020', '10-04-2020', '13:45', "
+                           "'155.00', 'booked', NULL);")
+            cursor.execute("INSERT IGNORE INTO `bookings` VALUES (NULL, 1, 2, '10-03-2020', '15-03-2020', '13:45', "
+                           "'123.00', 'Canceled', NULL);")
 
         self.connection.commit()
 
     def insert_account(self, firstname, lastname, email, password):
         with self.connection.cursor() as cursor:
-            cursor.execute("INSERT INTO customers VALUES (NULL, %s, %s, %s, %s)", (firstname, lastname, email, password))
+            cursor.execute("INSERT INTO customers VALUES (NULL, %s, %s, %s, %s)",
+                           (firstname, lastname, email, password))
         self.connection.commit()
 
     def check_exist_username(self, email):
         cursor = self.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT * FROM customers WHERE email = %s', (email))
+        cursor.execute('SELECT * FROM customers WHERE email = %s', email)
 
         return cursor.fetchone()
 
@@ -103,9 +109,9 @@ class DatabaseUtils:
 
         return cursor.fetchone()
 
-    def get_an_user(self, username):
+    def get_an_user(self, email):
         cursor = self.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM Cust_User WHERE username = %s", (username))
+        cursor.execute("SELECT * FROM customers WHERE email = %s", email)
 
         return cursor.fetchone()
 
@@ -115,36 +121,58 @@ class DatabaseUtils:
 
         return cursor.fetchall()
 
-    def get_customer_booking_history(self, customerid):
+    # show a list of cars that current user has booked
+    def get_customer_booking_history(self, customer_id):
         cursor = self.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM bookings b INNER JOIN cars_list c ON b.car_id = c.car_id")
-
-        # cursor.execute("SELECT bookings.*, cars_list.* FROM bookings b INNER JOIN cars_list c ON b.car_id = c.car_id WHERE user_id = %s", (customerid))
-
-        # SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
-        # FROM Orders
-        # INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+        cursor.execute("SELECT b.booking_id, c.first_name, c.last_name, b.start_date, b.end_date, b.pickup_time, "
+                       "b.booking_amount, b.booking_status, cl.make_name, cl.model_name "
+                       "FROM customers c "
+                       "JOIN bookings b on c.customer_id = b.customer_id JOIN "
+                       "cars_list cl on b.car_id = cl.car_id "
+                       "WHERE c.customer_id = %s "
+                       "ORDER BY b.start_date DESC",
+                       customer_id)
 
         return cursor.fetchall()
 
-    def get_available_car(self, car_type):
+    def get_all_available_car_type(self, car_type):
+        print(type(car_type))
         cursor = self.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM cars_list WHERE status = 1 and car_type = %s", car_type)
+        cursor.execute("SELECT * FROM cars_list WHERE status = 'Available' and car_type = %s", car_type)
 
         return cursor.fetchall()
 
-    def update_car(self, car_id):
-        cursor = self.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("UPDATE Cars_list SET status = 2 WHERE car_id = %s", car_id)
-
-        self.connection.commit()
-
-    def insert_booking(self, userId, car_id, startDate, endDate, _time):
+    def insert_booking(self, customer_id, car_id, startDate, endDate, _time, booking_status, booking_amount):
         with self.connection.cursor() as cursor:
-            cursor.execute("INSERT INTO Booking(username,car_id,start_date,end_date,pickup_time) "
-                           "VALUES (%s,%s,%s,%s,%s)",
-                           (userId, car_id, startDate, endDate, _time))
-            cursor.execute("UPDATE Booking SET booking_status = 1 WHERE car_id = %s", car_id)
+            cursor.execute("INSERT INTO bookings (customer_id, car_id, start_date, end_date, pickup_time, "
+                           "booking_status, booking_amount) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+                           (customer_id, car_id, startDate, endDate, _time, booking_status, booking_amount))
         self.connection.commit()
 
+    # Can you simplify the queries here ??????????????????????????????????????
+    def update_booking(self, customer_id):
+        cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+        # update the booking status to 'cancelled'
+        cursor.execute("UPDATE bookings "
+                       "SET booking_status = 'cancelled' "
+                       "WHERE customer_id = %s",
+                       customer_id)
 
+        # Need to fix here since it will get 2 car_id and thus, CANNOT update car status
+        car_id = cursor.execute("SELECT car_id FROM bookings WHERE customer_id = %s", customer_id)
+
+        # update the car status to 'Available'
+        cursor.execute("UPDATE cars_list "
+                       "SET status = 'Available' "
+                       "WHERE car_id = %s",
+                       car_id)
+
+        self.connection.commit()
+
+        # cursor.execute("UPDATE cars_list, bookings "
+        #                "SET b.booking_status = 'cancelled', cl.status = 'Available' "
+        #                "FROM customers c "
+        #                "JOIN bookings b on c.customer_id = b.customer_id JOIN "
+        #                "cars_list cl on b.car_id = cl.car_id "
+        #                "WHERE c.customer_id = %s",
+        #                customer_id)
--- main.py
+++ main.py
@@ -22,9 +22,11 @@ def login():
         email = request.form['username']
         password = request.form['password']
 
+        # query the account from the database by passing the input username and password
         account = db.login_account(email, password)
 
-        # If account exists in accounts table in out database
+        # Check if the account exists in the accounts table of database
+        # If account exists, redirect to home page
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
@@ -34,6 +36,8 @@ def login():
             session['email'] = account['email']
             # Redirect to home page
             return redirect(url_for('home'))
+
+        # If account doesn't exist, show the error message and pass it to index.html
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
@@ -51,7 +55,7 @@ def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-    # Redirect to login page
+    # Redirect to the login page
     return redirect(url_for('login'))
 
 
@@ -60,7 +64,7 @@ def logout():
 # http://localhost:5000/carrental/register - this will be the registration page,
 # we need to use both GET and POST requests
 @app.route('/register', methods=['GET', 'POST'])
-@app.route('/carrental/register', methods=['GET', 'POST'])
+@app.route('/carrental/register', methods=['GET', 'POST'])  # why there are 2 routes
 def register():
     # Output message if something goes wrong...
     msg = ''
@@ -76,16 +80,17 @@ def register():
         print(account)
         # If account exists show error and validation checks
         if account:
-            msg = 'Account already exists!'
+            msg = 'Account has already existed!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address!'
-        # elif not re.match(r'[A-Za-z0-9]+', email):
-        #     msg = 'Username must contain only characters and numbers!'
         elif not firstname or not lastname or not email or not password:
             msg = 'Please fill out the form!'
+
+        # if account doesn't existed, show the successful message and back to the login page
         else:
             db.insert_account(firstname, lastname, email, password)
             msg = 'You have successfully registered!'
+            return render_template('index.html', msg=msg)
 
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
@@ -98,117 +103,157 @@ def register():
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
-        # User is loggedin show them the home page
+        # User is logged in show them the home page
         return render_template('home.html', username=session['firstname'])
-    # User is not loggedin redirect to login page
+    # User is not logged in and redirect to login page
     return redirect(url_for('login'))
 
 
 # --------------------------------------------PROFILE PAGE-----------------------------------------------------
 
-# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
+# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for logged in users
 @app.route('/pythonlogin/profile')
 def profile():
-    # Check if user is loggedin
+    # Check if user is logged in
     if 'loggedin' in session:
-        account = db.get_an_user(session['username'])
+        account = db.get_an_user(session['email'])
 
         # Show the profile page with account info
-        return render_template('profile.html', account=account)
+        return render_template('profile.html', account=account, username=session['firstname'])
 
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
 
-# --------------------------------------------BOOKING PAGE-----------------------------------------------------
+# --------------------------------------------SEARCH PAGE-----------------------------------------------------
 
-# http://localhost:5000/carrental/booking - this will be the home page, only accessible for loggedin users
-@app.route('/carrental/booking/', methods=['GET', 'POST'])
+# http://localhost:5000/carrental/search - this will be the search page, only accessible for loggedin users
+@app.route('/carrental/search/', methods=['GET', 'POST'])
+def search():
+
+    # Check if user is logged in and has filled the form
+    if 'loggedin' in session and 'carType' in request.form and 'startDate' in request.form \
+            and 'endDate' in request.form and 'pickupTime' in request.form:
+
+        # check the availability of the Car by getting the value of the selection in new_booking.html file
+        cars = db.get_all_available_car_type(int(request.form.get('carType')))
+        print(cars)
+        if not cars:
+            flash("The car type that you have selected is currently NOT available")
+            return redirect(url_for('search'))
+        elif cars:
+            # Create session data, we may access these data in booking routes for future booking
+            session['car_type'] = int(request.form.get('carType'))
+            session['start_Date'] = request.form.get('startDate')
+            session['end_Date'] = request.form.get('endDate')
+            session['pick_up_Time'] = request.form.get('pickupTime')
+            session['booking_status'] = 'booked'
+            session['car_id'] = 888  # don't know how to fetch a particular car_id from searching result
+
+        # reload the new_booking page with showing the searching result
+        return render_template('new_booking.html', username=session['firstname'], cars=cars)
+
+    # Check if user has logged in ONLY
+    elif 'loggedin' in session:
+        return render_template('new_booking.html')
+
+    else:
+        # User is not logged in and redirect to login page
+        flash("You have been logged out. Please login again.")
+        return redirect(url_for('login'))
+
+# --------------------------------------------BOOK THE CAR PAGE-----------------------------------------------------
+
+# http://localhost:5000/carrental/booking - this will be the booking page, only accessible for logged in users
+@app.route('/carrental/booking', methods=['GET', 'POST'])
 def booking():
-    # Get input from the user
-    # then search cars
-    # fetch cars
-    if request.method == 'POST':
-        # Check if user is logged in
-        if 'loggedin' in session:
-            # account = db.get_an_user(session['username'])
-            
-            print(request.form)
-            car_type = request.form.get('carType')  # this grab the value of the selection in booking.html file
-            start_date = request.form.get('startDate')
-            end_Date = request.form.get('endDate')
-            pickup_time = request.form.get('pickupTime')
-            
+    # Check if user is logged in
+    if 'loggedin' in session:
+        session['booking_status'] = 'booked'
 
-            # check the availability of the Car
-            cars = db.get_available_car(car_type)
-            print(cars)
-            if not cars:
-                flash("The Car is currently not available")
-                return redirect(url_for('booking'))
+        db.insert_booking(session['id'],
+                          session['car_id'],
+                          session['start_Date'],
+                          session['end_Date'],
+                          session['pick_up_Time'],
+                          session['booking_status'],
+                          123.00)
+
+        # Remove session data after inserting a new booking
+        session.pop('id', None)
+        session.pop('car_id', None)
+        session.pop('start_Date', None)
+        session.pop('loggedin', None)
+        session.pop('end_Date', None)
+        session.pop('booking_status', None)
+
+        # User is logged in show them the home page
+        return render_template('booking.html', username=session['firstname'])
+
+    # User is not logged in and redirect to login page
+    else:
+        return redirect(url_for('login'))
 
 
-            # pick_up_location = request.form["pickupLocation"]
-            # drop_off_location = request.form["dropoffLocation"]
-
-            # check the availability of the Car
-            # car_id = db.get_available_car(car_type)
-            # if not car_id:
-            #     flash("The Car is currently not available")
-            #     return redirect(url_for('booking'))
-
-            # db.update_car(car_id)
-            # db.insert_booking(session['username'], car_id, start_date, end_Date, _time)
-
-            # global CARID
-            # CARID = carid[:]
-            # global b_actual_id
-
-            # Show the booking page
-            return render_template('new_booking.html', username=session['firstname'])
-
-        else:
-            # User is not logged in and redirect to login page
-            flash("You have been logged out. Please login again.")
-            return redirect(url_for('login'))
-
-    return render_template('new_booking.html', username=session['firstname'])
 
 # --------------------------------------------CAR LIST PAGE-----------------------------------------------------
 
-# http://localhost:5000/carrental/profile - this will be the Cars List page, only accessible for loggedin users
+# http://localhost:5000/carrental/carslist - this will be the Cars List page, only accessible for logged in users
 @app.route('/carrental/carslist')
 def carslist():
-    # Check if user is loggedin
+    # Check if user is logged in
     if 'loggedin' in session:
-
-        # check the availability of the Cars List
+        # get and show the list of all available cars
         carlist = db.get_all_available_cars()
 
         # Show the Cars List
         return render_template('cars.html', username=session['firstname'], cars=carlist)
 
-    # User is not loggedin redirect to login page
+    # User is not logged in redirect to login page
     return redirect(url_for('login'))
+
 
 # --------------------------------------------BOOKING HISTORY PAGE-----------------------------------------------------
 
-# http://localhost:5000/carrental/profile - this will be the Cars List page, only accessible for loggedin users
+# http://localhost:5000/carrental/bookinghistory - this will be the booking history page,
+# only accessible for logged in users
 @app.route('/carrental/bookinghistory')
 def bookinghistory():
     # Check if user is loggedin
     if 'loggedin' in session:
-
         customerid = session['id']
 
         # check the booking history of logged in customer
-        bookingshistory = db.get_customer_booking_history(customerid)
-        print(bookingshistory)
+        booking_history = db.get_customer_booking_history(customerid)
+        print(booking_history)
 
         # Show the Cars List
-        return render_template('booking_history.html', username=session['firstname'], bookings=bookingshistory)
+        return render_template('booking_history.html', username=session['firstname'], history=booking_history)
 
-    # User is not loggedin redirect to login page
+    # User is not logged in redirect to login page
+    return redirect(url_for('login'))
+
+
+# --------------------------------------------CANCEL BOOKING PAGE-----------------------------------------------------
+
+# http://localhost:5000/carrental/cancelbooking - this will be the cancel booking page,
+# only accessible for logged in users
+@app.route('/carrental/bookinghistory/cancelbooking', methods=['GET', 'POST'])
+def cancel_booking():
+    # Check if user is logged in
+    if 'loggedin' in session:
+        customerid = session['id']
+        # update the booking status to 'cancel' and set car's status to 'Available'
+        db.update_booking(customerid)
+
+        # check the booking history of logged in customer
+        booking_history = db.get_customer_booking_history(customerid)
+        print(booking_history)
+
+        flash("The booking is successfully cancelled!!!")
+        return render_template('booking_history.html', username=session['firstname'], history=booking_history)
+
+    # User is not logged in redirect to login page
     return redirect(url_for('login'))
 
 
