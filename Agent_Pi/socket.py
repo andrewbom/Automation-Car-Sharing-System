072680925488f0354751ae6e4ce8244946f94466
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-05-08 22:52:00 +1000

    update

---
+++ Agent_Pi/socket.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import sys
+import datetime
+from getpass import getpass
+
+
+# Wait for incoming data from server
+# .decode is used to turn the message in bytes to a string
+def receive(socket, signal):
+    while signal:
+        try:
+            data = socket.recv(32)
+            print(str(data.decode("utf-8")))
+        except:
+            print("You have been disconnected from the server")
+            signal = False
+            break
+
+
+# Get host and port
+# host = input("Host:localhost")
+# port = int(input("Port:8080"))
+
+# Attempt connection to server
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1", 5001))
+except:
+    print("Could not make a connection to the server")
+    input("Press enter to quit")
+    sys.exit(0)
+
+# Create new thread to wait for data
+receiveThread = threading.Thread(target=receive, args=(sock, True))
+receiveThread.start()
+
+# Send data to server
+# str.encode is used to turn the string message into bytes so it can be sent across the network
+while True:
+    # message = '{"customer_id":"6","car_id":"1","username":"abc@gmail.com","password":"123"}'
+    username = input("Insert Username :")
+    print("\n")
+
+    password = getpass("Insert password :")
+    print("\n")
+
+    customer_id = input("Insert your Customer ID :")
+    print("\n")
+
+    car_id = input("Insert Car ID :")
+    print("\n")
+
+    message = '{"username":"%s","password":"%s","customer_id":"%s","car_id":"%s"}' % (
+        username, password, customer_id, car_id)
+    print("\n")
+    sock.sendall(str.encode(message))
---
+++ Master_Pi/server.py
@@ -0,0 +1,95 @@
+import socket
+import threading
+from database_utils import DatabaseUtils
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Variables for holding information about connections
+connections = []
+total_connections = 0
+
+
+class ServerClass:
+    # Client class, new instance created for each connected client
+    # Each instance has the socket and address that is associated with items
+    # Along with an assigned ID and a name chosen by the client
+    class Client(threading.Thread):
+        def __init__(self, socket, address, id, name, signal):
+            threading.Thread.__init__(self)
+            self.socket = socket
+            self.address = address
+            self.id = id
+            self.name = name
+            self.signal = signal
+
+        def __str__(self):
+            return str(self.id) + " " + str(self.address)
+
+        # Attempt to get data from client
+        # If unable to, assume client has disconnected and remove him from server data
+        # If able to and we get data back, print it in the server and send it back to every
+        # client aside from the client that has sent it
+        # .decode is used to convert the byte data into a printable string
+        def run(self):
+            while self.signal:
+                try:
+                    data = self.socket.recv(100)
+                except:
+                    print("Client " + str(self.address) + " has disconnected")
+                    self.signal = False
+                    connections.remove(self)
+                    break
+                if data != "":
+                    # print("ID " + str(self.id) + ": " + str(data.decode("utf-8")))
+                    newdata = json.loads(data)
+                    # date = newdata["date"]
+                    username = newdata["username"]
+                    password = newdata["password"]
+                    customer_id = newdata["customer_id"]
+                    car_id = newdata["car_id"]
+
+                    db = DatabaseUtils()
+                    userdata = db.login_account(username, password)
+
+                    if (userdata is not None):
+                        verify = db.validate_collection(customer_id, car_id)
+                        if (verify is not None):
+                            for client in connections:
+                                if client.id == self.id:
+                                    client.socket.send("unlocking accepted")
+                        else:
+                            for client in connections:
+                                if client.id == self.id:
+                                    client.socket.send("unlocking denied")
+
+                    else:
+                        for client in connections:
+                            if client.id == self.id:
+                                client.socket.send("unlocking denied")
+
+    # Wait for new connections
+    def newConnections(self, socket):
+        while True:
+            sock, address = socket.accept()
+            global total_connections
+            connections.append(self.Client(sock, address, total_connections, "Name", True))
+            connections[len(connections) - 1].start()
+            print("New connection at ID " + str(connections[len(connections) - 1]))
+            total_connections += 1
+
+    def Serve(self):
+        # Get host and port
+        # host = input("Host: ")
+        # port = int(input("Port: "))
+
+        # Create new server socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(("127.0.0.1", 5001))
+        sock.listen(5)
+
+        # Create new thread to wait for connections
+        newConnectionsThread = threading.Thread(target=self.newConnections, args=(sock,))
+        newConnectionsThread.start()
