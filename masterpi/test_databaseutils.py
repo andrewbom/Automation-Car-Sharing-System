f092a2f6328e24ad7cbf9bb39dbcec20f4e28d13
andrewbom <61341845+andrewbom@users.noreply.github.com>
2020-07-22 22:46:55 +1000

    Update test_databaseutils.py

--- masterpi/test_databaseutils.py
+++ masterpi/test_databaseutils.py
@@ -6,9 +6,9 @@ from database_utils import DatabaseUtils
 
 
 class TestDatabaseUtils(unittest.TestCase):
-    HOST = "35.201.23.126"  # google cloud IP address
-    USER = "root"  # google cloud sql user name
-    PASSWORD = "andrewishandsome"  # google cloud sql password
+    HOST = ""  # google cloud IP address
+    USER = ""  # google cloud sql user name
+    PASSWORD = ""  # google cloud sql password
     DATABASE = "Pythonlogin"
 
     def setUp(self):
